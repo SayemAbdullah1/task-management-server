04c919770ed6c9d87d792be54b6bbf60cb7c0e85
SayemAbdullah1 <dxsayem0@gmail.com>
2022-12-29 20:25:53 +0600

    add delete function

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run(){
             const result = await taskCollection.insertOne(tasks)
             res.send(result)
         })
-        app.delete('/deleteTask/:id', async (req, res) => {
+        app.delete('/allTask/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const result = await taskCollection.deleteOne(filter)
